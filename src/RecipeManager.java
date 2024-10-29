887ef57db2ec5d7a2b27d90605852199747b7a1d
JamWild96 <100748711+JamWild96@users.noreply.github.com>
2024-10-28 21:12:29 -0700

    Updated src folder and files

---
+++ src/RecipeManager.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecipeManager {
+	private Database database;
+	
+	public RecipeManager(Database database) {
+		this.database = database;
+	}
+	
+	public List<Recipe> searchRecipes(String keyword) {
+		return database.queryRecipes(keyword);
+	}
+	
+	public void addRecipe(Recipe recipe) {
+		database.save(recipe);
+	}
+	
+	public void editRecipe(int recipeID, Recipe updatedRecipe) {
+		database.update(recipeID, updatedRecipe);
+	}
+	
+	public void deleteRecipe(int recipeID) {
+		database.delete(recipeID);
+	}
+}
+
+class Database {
+	public void connect() {
+		// Connect to the database
+	}
+	
+	public List<Recipe> queryRecipes(String keyword) {
+		return new ArrayList<>();
+	}
+	
+	public void save(Object data) {
+		// Save data to the database
+	}
+	
+	public void update(int id, Object data) {
+		// Update data in the database
+	}
+	
+	public void delete(int id) {
+		// Delete data from the database
+	}
+}
