99719932f4b6a161c847d4e103f3525c09e57f15
Anna Cubine <acubine10@gmail.com>
2024-11-06 21:11:16 -0800

    Refectored again and mapped it to the categories.html file

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,8 +1,13 @@
 package controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
+    @GetMapping("/")
+    public String index() {
+        return "categories";
+    }
 }
