28adecda3dca6d747a4e6d3d48582a1bb6c91122
ParkerMillCWU <MillerPar@cwu.edu>
2024-11-24 20:15:01 -0800

    created UserRepository and UserService

---
+++ src/main/java/repositories/UserRepository.java
@@ -0,0 +1,50 @@
+package repositories;
+
+import models.Users;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    /**
+     * Adding user to database using SQL query and jdbcTemplate
+     * @param user Taking in a Users model to add to database
+     */
+    public void addUser(Users user) {
+        String query = "INSERT INTO Users (userID, username, password, email) VALUES (?,?,?,?) ";
+        jdbcTemplate.update(query,
+                user.getUserID(),
+                user.getUsername(),
+                user.getPassword(),
+                user.getEmail()
+                );
+    }
+
+    /**
+     * Getting all recipes from the database using SQL query and jdbcTemplate
+     * @return The query along with mapped recipe model.
+     */
+    public List<Users> findUser() {
+        String query = "SELECT * FROM Users ";
+        return jdbcTemplate.query(query, userMapper());
+    }
+
+    public RowMapper<Users> userMapper() {
+        return (rs, rowNum) -> new Users(
+                rs.getInt("UserID"),
+                rs.getString("username"),
+                rs.getString("password"),
+                rs.getString("email")
+        );
+    }
+}
---
+++ src/main/java/services/UserService.java
@@ -0,0 +1,29 @@
+package services;
+
+import models.Users;
+import org.springframework.stereotype.Repository;
+import repositories.UserRepository;
+
+import java.util.List;
+
+@Repository
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService( UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * Saving user from the view and main controller down to the repository
+     * where it will be stored in the database
+     * @param user Taking the populated Users model
+     */
+    public void addUser(Users user) {
+        userRepository.addUser(user);
+    }
+
+    public List<Users> findUser() {
+        return userRepository.findUser();
+    }
+}
